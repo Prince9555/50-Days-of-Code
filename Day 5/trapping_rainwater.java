f85ccd5ec7ab9cff1e34b9c35423599d9245303d
Prince9555 <131245773+Prince9555@users.noreply.github.com>
2024-02-14 20:51:52 +0530

    Add files via upload

---
+++ Day 5/trapping_rainwater.java
@@ -0,0 +1,30 @@
+class Solution {
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int  arr[], int n) {
+        int left = 0;
+        int right = n - 1;
+        int left_max = 0;
+        int right_max = 0;
+        long water_trapped = 0;
+
+        while (left < right) {
+            if (arr[left] < arr[right]) {
+                if (arr[left] > left_max) {
+                    left_max = arr[left];
+                } else {
+                    water_trapped += left_max - arr[left];
+                }
+                left++;
+            } else {
+                if (arr[right] > right_max) {
+                    right_max = arr[right];
+                } else {
+                    water_trapped += right_max - arr[right];
+                }
+                right--;
+            }
+        }
+
+        return water_trapped;
+    }
+}
