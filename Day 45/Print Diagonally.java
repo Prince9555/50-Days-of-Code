96e402faa4fd3d075b6395ba152f06af201dba8a
Prince <princejaiswal13206@gmail.com>
2024-03-25 21:22:33 +0530

    Day 45

---
+++ Day 45/Print Diagonally.java
@@ -0,0 +1,12 @@
+
+Time (IST)	Status	Marks	Lang	Test Cases	Code
+2024-03-21 09:59:17	Correct	
+2
+java	112 / 112	View
+2024-03-21 09:58:56	Wrong	
+0
+java	2 / 112	View
+2024-03-21 09:57:48	Compilation Error	
+0
+java	0 / 112	View
+
